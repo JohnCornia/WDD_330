1923e1c3bff99b8527e0f7b4e6bb12cc5a659c2b
John Cornia <john.cornia.1@gmail.com>
2021-10-19 15:59:36 -0600

    added w5 and new w6 files

---
+++ weekSix/toDo.js
@@ -0,0 +1,17 @@
+//make a NodeJS module using C++ for the win
+
+document.getElementById("newTask").addEventListener("click", function() {
+    //append an element to the form
+  });
+
+  document.getElementById("removeTask").addEventListener("click", function() {
+    //remove a task from task list
+  });
+
+  document.getElementById("completeTask").addEventListener("click", function() {
+    //mark an element as complete
+  });
+
+  document.getElementById("filter").addEventListener("click", function() {
+    //pass in a parameter on how you want to filter the elements of the task
+  });
