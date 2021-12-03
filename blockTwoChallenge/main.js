152e1ecbc0773b745e9b1d2ee299504fd82253c2
John Cornia <john.cornia.1@gmail.com>
2021-12-03 11:21:16 -0700

    adding modal code

--- blockTwoChallenge/main.js
+++ blockTwoChallenge/main.js
@@ -1,8 +1,34 @@
-document.getElementById("position").addEventListener("change", function() {
-    console.log("Event listner called");
-    console.log(this);
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function display(elementId, data) {
+    console.log(elementId);
+    if (elementId == "sideMount" || elementId == "kneeMount" || elementId == "backMount" || elementId == "fullMount") {
+        modal.style.display = "block";
+        //console.log(data);
+    } else
+        modal.style.display = "none";
+}
+
+document.addEventListener('click', function(e) {
     fetch('https://johncornia.github.io/jiujitsuAPI/escapes.JSON')
         .then(response => response.json())
-        .then(data => console.log(data));
-
-})
+        //.then(console.log(response));
+        //.then(data => console.log(data));
+        .then(data => display(e.target.id, data));
+});
