7b555b9885019cb4fb20281429e96dbe0c6f975a
John Cornia <john.cornia.1@gmail.com>
2021-12-03 09:36:07 -0700

    creating pages to link to

--- blockTwoChallenge/main.js
+++ blockTwoChallenge/main.js
@@ -1,8 +1,8 @@
-/*document.getElementById("position").addEventListener("change", function () {
+document.getElementById("position").addEventListener("change", function() {
     console.log("Event listner called");
     console.log(this);
     fetch('https://johncornia.github.io/jiujitsuAPI/escapes.JSON')
-  .then(response => response.json())
-  .then(data => console.log(data));
+        .then(response => response.json())
+        .then(data => console.log(data));
 
-})*/
+})
